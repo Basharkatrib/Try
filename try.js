a479e6dfef433fadc5da8b57aadb88394d2d72b6
Your Name <you@example.com>
2024-11-22 12:31:58 +0300

    Firt Commit

---
+++ try.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper = new Swiper(".mySwiper2", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
